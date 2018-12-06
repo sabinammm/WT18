a52efa2f39235dee4c1efde4cf5883e045dc9d73
sabinammm <isobel@posteo.de>
2018-12-06 10:02:00 +0100

    Aufgabe 7

---
+++ Aufgabe7/aufgabe7.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+</head>
+<body>
+  <div  class="container-fluid">
+<h1>Anmeldung</h1>
+<form action="./aufgabe7.php" method="post" class="form-horizontal">
+  <div class="form-group">
+    <label for="vorname">Vorname : </label>
+    <input type="text" name="vorname" placeholder="Vorname">
+  </div>
+
+  <div class="form-group">
+    <label for="nachname">Nachname : </label>
+    <input type="text" name="nachname" placeholder="Nachname">
+  </div>
+
+
+  <button type="submit">Anmelden</button>
+</form>
+</div>
+
+
+</body>
+</html>
