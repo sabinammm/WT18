104018c660790cc30a072c65b3ec3c36ad4e0acf
sabinammm <isobel@posteo.de>
2018-12-06 11:04:07 +0100

    Aufgabe 7

--- Aufgabe7/aufgabe7.php
+++ Aufgabe7/aufgabe7.php
@@ -9,15 +9,28 @@
   <div class="container">
     <h1>Anmeldung</h1>
     <?php
-    IF($_POST) :
+    IF($_POST) {
 
       $vorname = filter_var($_POST["vorname"], FILTER_SANITIZE_STRING);
       $nachname = filter_var($_POST["nachname"], FILTER_SANITIZE_STRING);
       $email = filter_var($_POST["email"], FILTER_SANITIZE_EMAIL);
       $studiengang = filter_var($_POST["studiengang"], FILTER_SANITIZE_STRING); //eigentlich unnötig
 
+      $fehler = false;
+      if(!$vorname || $nachname || $studiengang) $fehler=true;
 
-
+      if(!$fehler)
+      {
+        echo "
+        <p>
+        Herzlichen Dank " . $vorname . " " . $nachname . " von Studiengang " . $studiengang . "!<br/>
+        Wir haben eine E-mail an " . $email . " gesendet. <br/>
+        <a href="./aufgabe7.php">zurück</a>
+        </p>
+        ";
+      }
+    }
+    if(($_POST && $fehler) || empty($_POST)) :
 
     ?>
     <p>
