b4c2515ceb3636885f298ab0b4fc601ba87d4887
Sabina Mollenhauer <s0563968@htw-berlin.de>
2019-04-01 18:35:24 +0200

    updated Aufgabe 5

--- Aufgabe5/aufgabe5.php
+++ Aufgabe5/aufgabe5.php
@@ -5,48 +5,71 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="styles4" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" />
   <title>Aufgabe 5</title>
+  <style type="text/css">
+  .page-header{
+    background-color: #FF7F50 !important;
+  }
+  .elemente{
+    background-color: #00BFFF;
+    }
+  </style>
   <?php
     function zufzahl($max, $anzahl)
     {
 
-      echo "<table>
-            <thead>
-            <tr>
+      echo "<table class='table-table-bordered'>";
+      echo "<thead>
             <th> Zufallszahl </th>
             <th>1 gerundet </th>
             <th>2 gerundet </th>
             <th>3 gerundet </th>
-            </tr>
-            </thead>
-            </tbody>";
+            </thead>";
+      echo "<tbody>";
       for($i=1; $i<=$anzahl; $i++)
       {
         $zzahl = rand(1,$max);
         $gerundet1 = abschneiden($zzahl,1);
         $gerundet2 = abschneiden($zzahl,2);
         $gerundet3 = abschneiden($zzahl,3);
-        echo "<tr>";
-        echo "<td>" .$zzahl. "</td>";
-        echo "<td>" .$gerundet1. "</td>";
-        echo "<td>" .$gerundet2. "</td>";
-        echo "<td>" .$gerundet3. "</td>";
-        echo "</tr>";
+
+        if ($zzahl>5000)
+        {
+          echo "<tr class='elemente' style='background-color:red;'>
+            <td>$zzahl</td>
+            <td>$gerundet1</td>
+            <td>$gerundet2</td>
+            <td>$gerundet3</td>
+          </tr>";
+      }
+      else {
+        echo "<tr class='elemente''>
+          <td>$zzahl</td>
+          <td>$gerundet1</td>
+          <td>$gerundet2</td>
+          <td>$gerundet3</td>
+        </tr>";
       }
     }
+    echo "</tbody>";
+  }
 
-    function abschneiden($zahl,$stellen=2)
-    {
-      $base = pow(10,$stellen);
-      return $zahl - ($zahl % $base);
-    }
-    ?>
+function abschneiden($zahl,$stellen=2)
+{
+  $base = pow(10,$stellen=2);
+  return $zahl - ($zahl % $base);
+}
+?>
 </head>
 <body>
-<!--  <div class="container-fluid"> -->
+   <div class="row page-header">
+     <div class="col-xl-1-12">
     <h1>Zufallszahlen</h1>
-    <div>
-      <?php zufzahl(20000, 20); ?>
     </div>
   </div>
+
+  <div>
+    <?php zufzahl(20000, 20); ?>
+  </div>
+
 </body>
 </html>
